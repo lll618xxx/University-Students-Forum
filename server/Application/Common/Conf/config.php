342a76d10b8d1e86828284b3b7078692d98813c6
lll618xxx <904799109@qq.com>
2019-08-04 19:17:03 +0800

    undpte

--- server/Application/Common/Conf/config.php
+++ server/Application/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_NAME'   => 'vues', // 数据库名
 	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '2394463zxc', // 密码
+	'DB_PWD'    => 'you password', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'vues_', // 数据库表前缀 
 	'DB_CHARSET'=> 'utf8', // 字符集
